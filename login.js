2b7cd998d05c22edbeebf41b8b66b06198870271
CaioSumizawa1 <105085021+CaioSumizawa1@users.noreply.github.com>
2023-12-01 18:41:35 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+function register(){
+
+}
+function validateLogin() {
+    const emailInput = document.getElementById('email').value;
+    const passwordInput = document.getElementById('password').value;
+
+
+    const fictitiousEmail = 'teste@example.com';
+    const fictitiousPassword = '123';
+
+    if (emailInput === fictitiousEmail && passwordInput === fictitiousPassword) {
+        window.location.href = "menu.html.html"; 
+        
+ } else {}
+}
